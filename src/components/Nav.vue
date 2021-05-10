eb96c874aae2c024b0ddcb535d1552d9d3ce8f98
Oriana Sanabria <orianasanabria2@gmail.com>
2021-05-09 21:53:15 -0400

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" :to="{ name: 'Home' }"
+        >Pizzería</router-link
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Home' }"
+                >Inicio</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Inventario' }"
+                >Inventario</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Carrito' }"
+                >Carrito</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Ventas' }"
+                >Ventas</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <li class="nav-item total">
+        <a class="text-light me-4">Total: ${{ totalCarrito }}</a>
+      </li>
+      <div @click="$router.push('/carrito')" class="cart-icon"></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Nav",
+  computed: {
+    ...mapGetters(["totalCarrito"]),
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.cart-icon {
+  width: 24px;
+  height: 24px;
+  line-height: 24px;
+  cursor: pointer;
+  &::before {
+    content: "shopping_cart";
+    font-family: "Material Icons";
+    font-size: 1.5rem;
+    color: #fff;
+  }
+}
+nav {
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+}
+.total {
+  list-style-type: none;
+  a {
+    text-decoration: none;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mt-5 pt-5">
+          <h1><em>Pizzería</em></h1>
+        </div>
+        <div class="col-12 mt-5">
+          <div class="pizza-card">
+            <div class="row">
+              <div
+                v-for="pizza in productosFiltrados"
+                :key="pizza.name"
+                class="col-4 mb-5"
+              >
+                <div class="card h-100">
+                  <img :src="pizza.img" class="card-img-top" alt="" />
+                  <div class="card-body">
+                    <h5 class="card-title text-capitalize">
+                      <strong>Pizza {{ pizza.name }}</strong>
+                    </h5>
+
+                    <p class="card-text">
+                      {{ pizza.desc }}
+                    </p>
+                    <ul class="list-group list-group-flush">
+                      <li
+                        v-for="(ing, i) in pizza.ing"
+                        :key="i"
+                        class="list-group-item text-capitalize"
+                      >
+                        {{ ing.name }}
+                      </li>
+                    </ul>
+                    <hr />
+                    <p class="text-center mb-3">
+                      <strong>Precio: ${{ pizza.price }}</strong>
+                    </p>
+
+                    <div class="row">
+                      <!-- <div class="col-6">
+                        <input
+                          v-model="cantidad"
+                          class="w-100 h-100"
+                          type="text"
+                        />
+                      </div> -->
+                      <div class="col-12">
+                        <button
+                          @click="
+                            agregarPizza({
+                              id: pizza.id,
+                              nombre: pizza.name,
+                              precio: pizza.price,
+                            })
+                          "
+                          class="btn btn-success w-100"
+                        >
+                          Agregar al carrito
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["productosFiltrados"]),
+  },
+  methods: {
+    ...mapMutations(["agregarPizza"]),
+  },
+};
+</script>
+
+<style></style>
